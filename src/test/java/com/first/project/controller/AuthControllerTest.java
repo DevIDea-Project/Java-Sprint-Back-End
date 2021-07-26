c2f59a0d4be58c2c0a23cb56bc6c44e739e9b6ff
Bruno Vinicius Felix Da Silva <brunoviniciusfelixdasilva@MacBook-Pro-de-Bruno.local>
2021-07-26 13:19:49 -0300

    Implemenst User

--- src/test/java/com/first/project/controller/AuthControllerTest.java
+++ src/test/java/com/first/project/controller/AuthControllerTest.java
@@ -1,43 +1,48 @@
-//package com.first.project.controller;
-//
-//import com.first.project.form.LoginForm;
-//import com.first.project.form.TokenForm;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@ActiveProfiles("test")
-//public class AuthControllerTest {
-//
-//    @Autowired
-//    private AuthController authController;
-//
-//    @org.junit.jupiter.api.Test
-//    public void deveriaRetornar400CasoDadosDeAutenticacaoEstejamErrados() {
-//
-//        LoginForm login = new LoginForm();
-//        login.setEmail("invalid@gmail.com");
-//        login.setSenha("123456");
-//
-//        ResponseEntity<TokenForm> token = authController.authentication(login);
-//
-//        assertEquals(HttpStatus.BAD_REQUEST, token.getStatusCode());
-//
-//    }
-//
-//
-//    @org.junit.jupiter.api.Test
+package com.first.project.controller;
+
+import com.first.project.form.LoginForm;
+import com.first.project.form.TokenForm;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class AuthControllerTest {
+
+    @Autowired
+    private AuthController authController;
+
+    @Test
+    public void deveriaRetornar400CasoDadosDeAutenticacaoEstejamErrados() {
+
+        LoginForm login = new LoginForm();
+        login.setEmail("invalid@gmail.com");
+        login.setSenha("123456");
+
+        ResponseEntity<TokenForm> token = authController.authentication(login);
+
+        assertEquals(HttpStatus.BAD_REQUEST, token.getStatusCode());
+
+    }
+
+
+//    @Test
 //    public void toReturn200Sucess() {
 //
 //        LoginForm login = new LoginForm();
-//        login.setEmail("priii@gmail.com");
+//        login.setEmail("brunoviniciustica@gmail.com");
 //        login.setSenha("123456");
 //
 //        ResponseEntity<TokenForm> token = authController.authentication(login);
@@ -45,5 +50,5 @@
 //        assertEquals(HttpStatus.OK, token.getStatusCode());
 //
 //    }
-//
-//}
+
+}
